f6203739d8a9c7baeceff6483329a1e06df26396
ashish <arjain18@gmail.com>
2022-03-19 22:09:27 +0530

    Setting up project

---
+++ Rest.Test.API/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Rest.Test.API
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
