3bbee4478a204a6482998a858483235d743bac8b
ashish <arjain18@gmail.com>
2022-03-20 20:30:37 +0530

    Started with first test method

---
+++ Rest.Test.API/Tests/APIChallenge.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using System;
+using System.Threading.Tasks;
+
+namespace Rest.Test.API
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public async Task TestMethod1Async()
+        {
+            RestClient client = new RestClient("https://apichallenges.herokuapp.com/challenger");
+            RestRequest request = new RestRequest("",Method.Post);
+            RestResponse restResponse = await client.PostAsync(request);
+            Console.WriteLine(restResponse.StatusCode);
+           Console.WriteLine(restResponse.StatusDescription);
+           
+        }
+    }
+}
