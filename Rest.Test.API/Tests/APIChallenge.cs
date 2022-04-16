a330e3760eca2b83e7235b3c6de122f73be60e88
Ashish Jain <arjain18@gmail.com>
2022-04-16 21:14:21 +0530

    Started with reading excel, primary stage

--- Rest.Test.API/Tests/APIChallenge.cs
+++ Rest.Test.API/Tests/APIChallenge.cs
@@ -3,7 +3,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Rest.Test.API.Helpers;
 using RestSharp;
 using System.Threading.Tasks;
-
+using OfficeOpenXml;
+using System.IO;
 
 namespace Rest.Test.API
 {
@@ -19,7 +20,27 @@ namespace Rest.Test.API
             RestResponse restResponse = await client.PostAsync(request);
 
            FrameworkHelper.checkAssert(201, (int)restResponse.StatusCode);
+            
 
+        }
+        
+        [TestMethod]
+        public void TestExcel()
+        {
+          FileInfo existingFile = new FileInfo($"C:\\Development\\Rest.Test.API\\Rest.Test.API\\TestData\\TestData.xlsx");
+			using (ExcelPackage package = new ExcelPackage(existingFile))
+			{
+                // get the first worksheet in the workbook
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+               
+           
+            string valA1 = worksheet.Cells["A1"].Value.ToString();
+            string valB1 = worksheet.Cells[1, 2].Value.ToString();
+
+            Report.print(valA1);
+            Report.print(valB1);
+            
+        }
         }
     }
 }
