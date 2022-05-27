f1fc2dd42588caafcf51407bdacd97b06ff2b71f
Ashish Jain <arjain18@gmail.com>
2022-05-27 07:49:18 +0530

    CSV started working, test case is added and its calling thhe values from CSV

---
+++ Rest.Test.API/TestDataDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rest.Test.API.Tests
+{
+    public class TestDataDto
+    {
+        public int SrNo { get; set; }
+        public int StatusCode { get; set; }    
+
+        public string? TestCaseName { get; set; }
+        public string? URL { get; set; }
+        public string? Method { get; set; }
+        public string? inputJson { get; set; }
+        public string? OutputJson { get; set; }
+
+    }
+}
--- Rest.Test.API/Tests/APIChallenge.cs
+++ Rest.Test.API/Tests/APIChallenge.cs
@@ -2,46 +2,60 @@
 using Rest.Test.API.Helpers;
 using RestSharp;
 using System.Threading.Tasks;
-using OfficeOpenXml;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
+using System;
+using CsvHelper;
+using System.Collections.Generic;
+using System.Globalization;
 
 
-namespace Rest.Test.API
+namespace Rest.Test.API.Tests
 {
     [TestClass]
     public class APIChallenges
     {
-private TestContext testContextInstance;
-public TestContext TestContextInstance
-{
-   get { return testContextInstance; }
-   set { testContextInstance = value; }
-}
+        
         [TestMethod]
         public async Task TestMethodPostAsync()
         {
             Report.print("TestCase Execution started");
             RestClient client = new RestClient("https://apichallenges.herokuapp.com/challenger");
-            RestRequest request = new RestRequest("",Method.Post);
+            RestRequest request = new RestRequest("", Method.Post);
             RestResponse restResponse = await client.PostAsync(request);
 
-           FrameworkHelper.checkAssert(201, (int)restResponse.StatusCode);
-            
+            FrameworkHelper.checkAssert(201, (int)restResponse.StatusCode);
+
 
         }
-        
+
 
         [TestMethod]
-        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
-         @"|DataDirectory|\TestData\TestData.csv", "Test1#csv",
-         DataAccessMethod.Sequential)]
-        public void Verify_ReadCSV()
+        [DynamicData(nameof(GetTestData), DynamicDataSourceType.Method)]
+        public void TestAddAJ(int SrNo, string TestCaseName, string URL, string Method, string inputJson, string OutputJson, int StatusCode)
         {
-            string _firstName = testContextInstance.DataRow[0].ToString();
-          //  string _lastName = TestContext.DataRow[1].ToString();
-         //   string _name = TestContext.DataRow[2].ToString();
-
+            Console.WriteLine(SrNo + StatusCode); 
+            //Assert.AreEqual(expected, actual);
+        }
+        public static IEnumerable<object[]> GetTestData()
+        {
+            IEnumerable<TestDataDto> data = ReadCsv();
+           
+                foreach(var item in data)
+                {
+                   
+                    yield return new object[]
+                    {
+                        item.SrNo, item.TestCaseName, item.URL, item.Method, item.inputJson, item.OutputJson, item.StatusCode
+                    };
+                
+                }
+        }
+        public static IEnumerable<TestDataDto> ReadCsv()
+        {
+            StreamReader? reader = new StreamReader("TestData\\Testdata.csv");
+            var csv = new CsvReader(reader, CultureInfo.InvariantCulture, false);
+            return csv.GetRecords<TestDataDto>();
         }
     }
 }
